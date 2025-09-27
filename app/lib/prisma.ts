1944de6ac58b407880b9da2ba57d07dfbc44d2f2
samuel <samuelolagoroye6@gmail.com>
2025-09-27 04:43:20 +0100

    Downgraded react from 19 to 18

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,7 +1,10 @@
 // app/lib/prisma.ts
 // import { PrismaClient } from "../generated/prisma/index.js";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
+// @ts-ignore
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();

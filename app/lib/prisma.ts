73e68417fd670361baf5abe06a5166ee05311a44
samuel <samuelolagoroye6@gmail.com>
2025-09-27 05:27:03 +0100

    Added turso db for production

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,8 @@
 // app/lib/prisma.ts
 // import { PrismaClient } from "../generated/prisma/index.js";
 // import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
@@ -9,19 +11,19 @@ let prisma: PrismaClient;
 
 if (process.env.TURSO_DATABASE_URL) {
   // Production on Vercel
-  const { createClient } = await import("@libsql/client");
-  const { PrismaLibSQL } = await import("@prisma/adapter-libsql");
 
   const libsql = createClient({
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   });
 
+  // @ts-ignore
   const adapter = new PrismaLibSQL(libsql);
   prisma = new PrismaClient({ adapter });
 } else {
   // Local dev fallback
   //   prisma = new PrismaClient();
+  // @ts-ignore
   const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
   prisma = globalForPrisma.prisma || new PrismaClient();
   if (process.env.NODE_ENV !== "production") {

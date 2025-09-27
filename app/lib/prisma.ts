5813999606bfc3568964a011c9c73a13058c737a
samuel <samuelolagoroye6@gmail.com>
2025-09-27 05:56:44 +0100

    Changed to neon db

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,34 +1,14 @@
 // app/lib/prisma.ts
 // import { PrismaClient } from "../generated/prisma/index.js";
 // import { PrismaClient } from "@prisma/client";
-import { createClient } from "@libsql/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-//@ts-ignore
-let prisma: PrismaClient;
+// @ts-ignore
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-if (process.env.TURSO_DATABASE_URL) {
-  // Production on Vercel
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-  const libsql = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  });
-
-  // @ts-ignore
-  const adapter = new PrismaLibSQL(libsql);
-  prisma = new PrismaClient({ adapter });
-} else {
-  // Local dev fallback
-  //   prisma = new PrismaClient();
-  // @ts-ignore
-  const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-  prisma = globalForPrisma.prisma || new PrismaClient();
-  if (process.env.NODE_ENV !== "production") {
-      globalForPrisma.prisma = prisma;
-  }
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
 }
-
-export { prisma };
